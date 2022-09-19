4ca31f9631e9fd8fdf751422bd80ce7c90288b66
Murat1ck <muratoff101088@gmail.com>
2022-09-19 20:42:26 +0300

    Задание №6

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+
+        var firstBoxer = 78.2;
+        var secondBoxer = 82.7;
+        var totalWeight = firstBoxer + secondBoxer;
+
+
+        System.out.println(totalWeight);
+    }
+}
